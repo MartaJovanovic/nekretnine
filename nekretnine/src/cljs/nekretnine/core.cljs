d81883e738533febdc76e01751e68950b01b4a4b
Marta Jovanovic <jovanovic99marta@gmail.com>
2022-09-08 12:25:09 +0200

    uveden re-frame

--- nekretnine/src/cljs/nekretnine/core.cljs
+++ nekretnine/src/cljs/nekretnine/core.cljs
@@ -1,9 +1,40 @@
 (ns nekretnine.core
-  (:require [reagent.core :as r]
-            [reagent.dom :as dom]
-            [ajax.core :refer [GET POST]]
-            [clojure.string :as string]
-            [nekretnine.validation :refer [validate-adresa]]))
+  (:require  [reagent.core :as r]
+             [reagent.dom :as dom]
+             [re-frame.core :as rf]
+             [ajax.core :refer [GET POST]]
+             [clojure.string :as string]
+             [nekretnine.validation :refer [validate-adresa]]))
+
+
+
+(rf/reg-event-fx
+ :app/initialize
+ (fn [_ _]
+   {:db {:adrese/loading? true}}))
+
+(rf/reg-sub
+ :adrese/loading?
+ (fn [db _]
+   (:adrese/loading? db)))
+
+(rf/reg-sub
+ :adrese/list
+ (fn [db _]
+   (:adrese/list db [])))
+
+(rf/reg-event-db
+ :adrese/set
+ (fn [db [_ adrese]]
+   (-> db
+       (assoc :adrese/loading? false
+              :adrese/list adrese))))
+
+(rf/reg-event-db
+ :adrese/add
+ (fn [db [_ adrese]]
+   (update db :adrese/list conj adrese)))
+
 
 
 
@@ -19,34 +50,32 @@
 
 
 
-(defn get-adrese [adrese]
+(defn get-adrese []
   (GET "/adrese"
     {:headers {"Accept" "application/transit+json"}
-     :handler #(reset! adrese (:adrese %))}))
+     :handler #(rf/dispatch [:adrese/set (:adrese %)])}))
 
 
 
 
 
-
-
-(defn send-adresa! [fields errors adrese]
-  (POST "/adresa"
-    {:format :json
-     :headers
-     {"Accept" "application/transit+json"
-      "x-csrf-token" (.-value (.getElementById js/document "token"))}
-     :params @fields
-     :handler (fn [_]
-                (swap! adrese conj (assoc @fields
-                                          :timestamp (js/Date.)))
-                (reset! fields nil)
-                (reset! errors nil))
-     :error-handler (fn [e]
-                      (.log js/console (str e))
-                      (reset! errors (-> e :response :errors)))}))
-
-
+(defn send-adresa! [fields errors]
+  (if-let [validation-errors (validate-adresa @fields)]
+    (reset! errors validation-errors)
+    (POST "/adresa"
+      {:format :json
+       :headers
+       {"Accept" "application/transit+json"
+        "x-csrf-token" (.-value (.getElementById js/document "token"))}
+       :params @fields
+       :handler (fn [_]
+                  (rf/dispatch
+                   [:adrese/add (assoc @fields :timestamp (js/Date.))])
+                  (reset! fields nil)
+                  (reset! errors nil))
+       :error-handler (fn [e]
+                        (.log js/console (str e))
+                        (reset! errors (-> e :response :errors)))})))
 
 
 (defn errors-component [errors id]
@@ -57,7 +86,8 @@
 
 
 
-(defn adresa-form [adrese]
+
+(defn adresa-form []
   (let [fields (r/atom {})
         errors (r/atom nil)]
     (fn []
@@ -82,21 +112,25 @@
                              assoc :adresa (-> % .-target .-value))}]]
        [:input.button.is-primary
         {:type :submit
-         :on-click #(send-adresa! fields errors adrese)
+         :on-click #(send-adresa! fields errors)
          :value "Dodaj"}]])))
 
 
 
 (defn home []
-  (let [adrese (r/atom nil)]
-    (get-adrese adrese)
+  (let [adrese (rf/subscribe [:adrese/list])]
+    (rf/dispatch [:app/initialize])
+    (get-adrese)
     (fn []
       [:div.content>div.columns.is-centered>div.column.is-two-thirds
-       [:div.columns>div.column
-        [:h3 "Adrese"]
-        [adresa-list adrese]]
-       [:div.columns>div.column
-        [adresa-form adrese]]])))
+       (if @(rf/subscribe [:adrese/loading?])
+         [:h3 "Ucitavanje adresa..."]
+         [:div
+          [:div.columns>div.column
+           [:h3 "Adrese"]
+           [adresa-list adrese]]
+          [:div.columns>div.column
+           [adresa-form]]])])))
 
 
 (dom/render
