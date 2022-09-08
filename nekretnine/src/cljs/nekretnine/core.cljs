c66e7ab582899ae1b19b4682056f0d4e1a54771f
Marta Jovanovic <jovanovic99marta@gmail.com>
2022-09-08 18:21:32 +0200

    dugme referesuj

--- nekretnine/src/cljs/nekretnine/core.cljs
+++ nekretnine/src/cljs/nekretnine/core.cljs
@@ -11,7 +11,18 @@
 (rf/reg-event-fx
  :app/initialize
  (fn [_ _]
-   {:db {:adrese/loading? true}}))
+   {:db {:adrese/loading? true}
+    :dispatch [:adrese/load]}))
+
+(rf/reg-event-fx
+ :adrese/load
+ (fn [{:keys [db]} _]
+   (GET "/api/adrese"
+     {:headers {"Accept" "application/transit+json"}
+      :handler #(rf/dispatch [:adrese/set (:adrese %)])})
+   {:db (assoc db :adrese/loading? true)}))
+
+
 
 (rf/reg-sub
  :adrese/loading?
@@ -35,6 +46,89 @@
  (fn [db [_ adrese]]
    (update db :adrese/list conj adrese)))
 
+(rf/reg-event-db
+ :form/set-field
+ [(rf/path :form/fields)]
+ (fn [fields [_ id value]]
+   (assoc fields id value)))
+
+(rf/reg-event-db
+ :form/clear-fields
+ [(rf/path :form/fields)]
+ (fn [_ _]
+   {}))
+
+(rf/reg-sub
+ :form/fields
+ (fn [db _]
+   (:form/fields db)))
+
+(rf/reg-sub
+ :form/field
+ :<- [:form/fields]
+ (fn [fields [_ id]]
+   (get fields id)))
+
+(rf/reg-event-db
+ :form/set-server-errors
+ [(rf/path :form/server-errors)]
+ (fn [_ [_ errors]]
+   errors))
+
+(rf/reg-sub
+ :form/server-errors
+ (fn [db _]
+   (:form/server-errors db)))
+
+;;Validation errors are reactively computed
+
+(rf/reg-sub
+ :form/validation-errors
+ :<- [:form/fields]
+ (fn [fields _]
+   (validate-adresa fields)))
+
+(rf/reg-sub
+ :form/validation-errors?
+ :<- [:form/validation-errors]
+ (fn [errors _]
+   (not (empty? errors))))
+
+(rf/reg-sub
+ :form/errors
+ :<- [:form/validation-errors]
+ :<- [:form/server-errors]
+ (fn [[validation server] _]
+   (merge validation server)))
+
+(rf/reg-sub
+ :form/error
+ :<- [:form/errors]
+ (fn [errors [_ id]]
+   (get errors id)))
+
+(rf/reg-event-fx
+ :adrese/send!
+ (fn [{:keys [db]} [_ fields]]
+   (POST "/api/adresa"
+     {:format :json
+      :headers
+      {"Accept" "application/transit+json"
+       "x-csrf-token" (.-value (.getElementById js/document "token"))}
+      :params fields
+      :handler #(rf/dispatch
+                 [:adrese/add
+                  (-> fields
+                      (assoc :timestamp (js/Date.)))])
+      :error-handler #(rf/dispatch
+                       [:form/set-server-errors
+                        (get-in % [:response :errors])])})
+   {:db (dissoc db :form/server-errors)}))
+
+
+
+
+
 
 
 
@@ -57,65 +151,56 @@
 
 
 
-
-
-(defn send-adresa! [fields errors]
-  (if-let [validation-errors (validate-adresa @fields)]
-    (reset! errors validation-errors)
-    (POST "/api/adresa"
-      {:format :json
-       :headers
-       {"Accept" "application/transit+json"
-        "x-csrf-token" (.-value (.getElementById js/document "token"))}
-       :params @fields
-       :handler (fn [_]
-                  (rf/dispatch
-                   [:adrese/add  (-> @fields
-                                     (assoc :timestamp (js/Date.))
-                                     (update :ime str " [CLIENT]"))])
-                  (reset! fields nil)
-                  (reset! errors nil))
-       :error-handler (fn [e]
-                        (.log js/console (str e))
-                        (reset! errors (-> e :response :errors)))})))
-
-
-(defn errors-component [errors id]
-  (when-let [error (id @errors)]
+(defn errors-component [id]
+  (when-let [error @(rf/subscribe [:form/error id])]
     [:div.notification.is-danger (string/join error)]))
 
 
 
 
 
-
 (defn adresa-form []
-  (let [fields (r/atom {})
-        errors (r/atom nil)]
-    (fn []
-      [:div
-       [errors-component errors :server-error]
-       [:div.field
-        [:label.label {:for :ime} "Ime"]
-        [errors-component errors :ime]
-        [:input.input
-         {:type :text
-          :name :ime
-          :on-change #(swap! fields
-                             assoc :ime (-> % .-target .-value))
-          :value (:ime @fields)}]]
-       [:div.field
-        [:label.label {:for :adresa} "Adresa"]
-        [errors-component errors :adresa]
-        [:textarea.textarea
-         {:name :adresa
-          :value (:adresa @fields)
-          :on-change #(swap! fields
-                             assoc :adresa (-> % .-target .-value))}]]
-       [:input.button.is-primary
-        {:type :submit
-         :on-click #(send-adresa! fields errors)
-         :value "Dodaj"}]])))
+  [:div
+   [errors-component  :server-error]
+   [:div.field
+    [:label.label {:for :ime} "Ime"]
+    [errors-component :ime]
+    [:input.input
+     {:type :text
+      :name :ime
+      :on-change #(rf/dispatch
+                   [:form/set-field
+                    :ime
+                    (.. % -target -value)])
+      :value @(rf/subscribe [:form/field :ime])}]]
+   [:div.field
+    [:label.label {:for :adresa} "Adresa"]
+    [errors-component :adresa]
+    [:textarea.textarea
+     {:name :adresa
+      :value  @(rf/subscribe [:form/field :adresa])
+      :on-change  #(rf/dispatch
+                    [:form/set-field
+                     :adresa
+                     (.. % -target -value)])}]]
+   [:input.button.is-primary
+    {:type :submit
+     :disabled @(rf/subscribe [:form/validation-errors?])
+     :on-click  #(rf/dispatch [:adrese/send!
+                               @(rf/subscribe [:form/fields])])
+     :value "Dodaj"}]])
+
+
+
+(defn reload-adrese-button []
+  (let [loading? (rf/subscribe [:adrese/loading?])]
+    [:button.button.is-info.is-fullwidth
+     {:on-click #(rf/dispatch [:adrese/load])
+      :disabled @loading?}
+     (if @loading?
+       "Ucitavanje adresa"
+       "Refresuj")]))
+
 
 
 
@@ -123,14 +208,13 @@
   (let [adrese (rf/subscribe [:adrese/list])]
     (fn []
       [:div.content>div.columns.is-centered>div.column.is-two-thirds
-       (if @(rf/subscribe [:adrese/loading?])
-         [:h3 "Ucitavanje adresa..."]
-         [:div
-          [:div.columns>div.column
-           [:h3 "Adrese"]
-           [adresa-list adrese]]
-          [:div.columns>div.column
-           [adresa-form]]])])))
+       [:div.columns>div.column
+        [:h3 "Adrese"]
+        [adresa-list adrese]]
+       [:div.columns>div.column
+        [reload-adrese-button]]
+       [:div.columns>div.column
+        [adresa-form]]])))
 
 (defn ^:dev/after-load mount-components []
   (rf/clear-subscription-cache!)
